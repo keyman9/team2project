17e25fb7dc80edc13d733e773577f7363e06535b
keyman9 <laxscott@gmail.com>
2016-03-21 15:42:22 +0000

    Added visiting postgres user

--- coffees.sql
+++ coffees.sql
@@ -1,7 +1,6 @@
 DROP DATABASE IF EXISTS coffee;
 CREATE DATABASE  coffee;
 \c coffee;
-
 --
 -- Table structure for table City
 --
@@ -19,12 +18,19 @@ CREATE TABLE Coffee (
 ) ;
 
 --
+-- Grant permissions
+--
+create user visiting with password '06*65uSl13Cu';
+grant all on coffee to visiting;
+grant all on coffee_id_seq to visiting;
+
+--
 -- Insert elements into Coffee table
 --
-INSERT INTO COFFEE(Name,Price,Weight,Roast,Region,Description) VALUES ("Aldo's Blend",14.00,12,"Medium Dark","Central America","Spiced cider, dark chocolate, earthy, mequite");
-INSERT INTO COFFEE(Name,Price,Weight,Roast,Region,Description) VALUES ("Bell's Blend",14.00,12,"Medium","Central America","Peanut butter, floral earth, hot cocoa with marshmallows, a happy childhood memory");
-INSERT INTO COFFEE(Name,Price,Weight,Roast,Region,Description) VALUES ("Decaf Monk",16.00,12,"Medium Dark","Central American","Red wine, smoked caramel, chocolate undertones");
-INSERT INTO COFFEE(Name,Price,Weight,Roast,Region,Description) VALUES ("Espresso Savio",14.00,12,"Medium Dark","Central America","Nutty, fresh citrus, toffee, milk chocolate");
-INSERT INTO COFFEE(Name,Price,Weight,Roast,Region,Description) VALUES ("Ethiopia Borboya",16.00,12,"Medium","Africa","Blackberry, lemongrass, and peach tea");
-INSERT INTO COFFEE(Name,Price,Weight,Roast,Region,Description) VALUES ("Fazenda Ambiental Fortaleza",16.00,12,"Medium","South America","Plum, fig, cumin, red fruit, tawny port, blood orange");
-INSERT INTO COFFEE(Name,Price,Weight,Roast,Region,Description) VALUES ("Finca Idealista",16.00,12,"Medium","Central America","Dark chocolate and ripe plums. Mellow finish");
+INSERT INTO Coffee(Name,Price,Weight,Roast,Region,Description) VALUES ('Aldo''s Blend',14.00,12,'Medium Dark','Central America','Spiced cider, dark chocolate, earthy, mequite');
+INSERT INTO Coffee(Name,Price,Weight,Roast,Region,Description) VALUES ('Bell''s Blend',14.00,12,'Medium','Central America','Peanut butter, floral earth, hot cocoa with marshmallows, a happy childhood memory');
+INSERT INTO Coffee(Name,Price,Weight,Roast,Region,Description) VALUES ('Decaf Monk',16.00,12,'Medium Dark','Central American','Red wine, smoked caramel, chocolate undertones');
+INSERT INTO Coffee(Name,Price,Weight,Roast,Region,Description) VALUES ('Espresso Savio',14.00,12,'Medium Dark','Central America','Nutty, fresh citrus, toffee, milk chocolate');
+INSERT INTO Coffee(Name,Price,Weight,Roast,Region,Description) VALUES ('Ethiopia Borboya',16.00,12,'Medium','Africa','Blackberry, lemongrass, and peach tea');
+INSERT INTO Coffee(Name,Price,Weight,Roast,Region,Description) VALUES ('Fazenda Ambiental Fortaleza',16.00,12,'Medium','South America','Plum, fig, cumin, red fruit, tawny port, blood orange');
+INSERT INTO Coffee(Name,Price,Weight,Roast,Region,Description) VALUES ('Finca Idealista',16.00,12,'Medium','Central America','Dark chocolate and ripe plums. Mellow finish');
--- server.py
+++ server.py
@@ -9,7 +9,7 @@ import psycopg2.extras
 app = Flask(__name__)
 
 def connectToDB():
-    connectionString = 'dbname=people user=radio password=12345 host=localhost'
+    connectionString = 'dbname=Coffee user=visiting password=06*65uSl13Cu host=localhost'
     print connectionString
     try: # there can be lots of errors early on, good to catch 'em. 
         return psycopg2.connect(connectionString)
