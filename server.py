312189c7586d6c0dd10d665b19d84d83e83eb0e0
hshami17 <hasanshami2013@gmail.com>
2016-03-26 16:29:41 -0400

    Changed DB name to lowercase 'coffee' to remove DB connect error

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 def connectToDB():
-    connectionString = 'dbname=Coffee user=visiting password=06*65uSl13Cu host=localhost'
+    connectionString = 'dbname=coffee user=visiting password=06*65uSl13Cu host=localhost'
     print connectionString
     try: # there can be lots of errors early on, good to catch 'em. 
         return psycopg2.connect(connectionString)
