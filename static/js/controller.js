9d9b01586602a7109388950842a702218b0464ea
hshami17 <hasanshami2013@gmail.com>
2016-04-13 01:22:59 -0400

    Adding socketIO to register page

--- static/js/controller.js
+++ static/js/controller.js
@@ -1,11 +1,29 @@
 var CoffeeCorner = angular.module('CoffeeCorner', []);
 
 CoffeeCorner.controller('FormFailedController', function($scope){
-	var socket = io.connect('http://' + document.domain + ':' + location.port);
+	var socket = io.connect('http://' + document.domain + ':' + location.port + '/coffee');
 
-	$scope.test = 'Text is here';
+	$scope.firstName = "";
+	$scope.lastName = "";
+	$scope.zipcode = "";
+	$scope.favCoffee = "";
+	$scope.username = "";
+	$scope.password = "";
+	$scope.passwordConf = "";
+
 
 	socket.on('connect', function(){
 		console.log('Connected');
 	});
+
+	socket.on('registerFail', function(msg){
+		console.log('REGISTER FAILED');
+		document.getElementById('message').textContent = msg;
+	});
+
+	$scope.register = function(){
+		console.log("here");
+		socket.emit('register', $scope.firstName, $scope.lastName, $scope.zipcode, 
+			$scope.favCoffee, $scope.username, $scope.password, $scope.passwordConf);
+	};
 });
